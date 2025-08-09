ba1acdafb27ad9b347dccd8e6263517ecf428dd8
GitVladislavHub <vladislav.drake31@gmail.com>
2025-08-09 19:20:43 +0300

    5.6 Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,23 +1,25 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
+
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    context = browser.new_context()
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    registration_email_input.fill('user.name@gmail.com')
+    page = context.new_page()
 
-    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    registration_username_input.fill('username')
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    registration_password_input.fill('password')
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_have_text('Dashboard')
-
+    context.storage_state(path="browser-state.json")

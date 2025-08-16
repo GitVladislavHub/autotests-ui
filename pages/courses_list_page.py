88782976956cdf0813caf57b02c4443c77b91c3b
GitVladislavHub <vladislav.drake31@gmail.com>
2025-08-16 13:56:38 +0300

    7.4 Практикуемся в реализации PageComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,6 +1,7 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.courses.course_view_component import CourseViewComponent
+from components.courses.courses_list_toolbar_view import CoursesListToolbarViewComponent
 from components.navigations.navbar_component import NavbarComponent
 from components.navigations.sidebar_component import SidebarComponent
 from components.views.empty_view_component import EmptyViewComponent
@@ -11,27 +12,15 @@ class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
+        self.navbar = NavbarComponent(page)
+        self.sidebar = SidebarComponent(page)
         self.empty_view = EmptyViewComponent(page, 'courses-list')
         self.course_view = CourseViewComponent(page)
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
 
-        self.sidebar = SidebarComponent(page)
-        self.navbar = NavbarComponent(page)
-
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
 
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title='There is no results',
             description='Results from the load test pipeline will be displayed here'
         )
-
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self):
-        self.create_course_button.click()

40aced6fd20e0a6b52e9c08953f1518022bd64ef
GitVladislavHub <vladislav.drake31@gmail.com>
2025-08-17 10:03:32 +0300

    7.4 Практикуемся в реализации PageComponent.Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -3,8 +3,8 @@ import pytest
 from pages.dashboard_page import DashBoardPage
 
 
-@pytest.mark.dashboard
 @pytest.mark.regression
+@pytest.mark.dashboard
 def test_dashboard_displaying(dashboard_page_with_state: DashBoardPage):
     dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
 
@@ -12,8 +12,9 @@ def test_dashboard_displaying(dashboard_page_with_state: DashBoardPage):
 
     dashboard_page_with_state.navbar.check_visible("username")
 
-    dashboard_page_with_state.check_visible_dashboard_title()
-    dashboard_page_with_state.check_visible_scores_chart()
-    dashboard_page_with_state.check_visible_courses_chart()
-    dashboard_page_with_state.check_visible_students_chart()
-    dashboard_page_with_state.check_visible_activities_chart()
+    dashboard_page_with_state.dashboard_title.check_visible()
+    dashboard_page_with_state.students_chart_view.check_visible("Students")
+    dashboard_page_with_state.activities_chart.check_visible("Activities")
+    dashboard_page_with_state.scores_chart_view.check_visible("Scores")
+    dashboard_page_with_state.courses_chart_view.check_visible("Courses")
+
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,4 @@
 import pytest
-from playwright.sync_api import Page
-
 from pages.dashboard_page import DashBoardPage
 from pages.registration_page import RegistrationPage
 
@@ -9,11 +7,10 @@ from pages.registration_page import RegistrationPage
 @pytest.mark.registration
 def test_successful_registration( dashboard_page: DashBoardPage, registration_page: RegistrationPage):
         registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-        registration_page.fill_registration_form(
+        registration_page.registration_form.fill_registration_form(
             email="email",
             username="username",
             password="password"
         )
         registration_page.click_registration_button()
-
-        dashboard_page.check_visible_dashboard_title()
+        dashboard_page.dashboard_title.check_visible()

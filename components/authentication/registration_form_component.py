37a33e4c132fd3d7d257cbf2ba1dd6488da7270d
GitVladislavHub <vladislav.drake31@gmail.com>
2025-08-19 14:46:57 +0300

    7.5 Знакомство с PageFactory. Практика

--- components/authentication/registration_form_component.py
+++ components/authentication/registration_form_component.py
@@ -1,26 +1,27 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from components.base_component import BaseComponent
+from elements.input import Input
 
 
 class RegistrationFormComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
-        self.email = page.get_by_test_id(f'{identifier}-email-input').locator('input')
-        self.username = page.get_by_test_id(f'{identifier}-username-input').locator('input')
-        self.password= page.get_by_test_id(f'{identifier}-password-input').locator('input')
+        self.identifier = identifier
+
+        self.email = Input(page, '{identifier}-email-input', 'Email')
+        self.username = Input(page, '{identifier}-username-input', 'Username')
+        self.password = Input(page, '{identifier}-password-input', 'Password')
 
     def fill_registration_form(self, email: str, password: str, username: str):
-        self.email.fill(email)
-        expect(self.email).to_have_value(email)
-        self.username.fill(username)
-        expect(self.username).to_have_value(username)
-        self.password.fill(password)
-        expect(self.password).to_have_value(password)
+        self.email.fill(email, identifier=self.identifier)
+        self.username.fill(username, identifier=self.identifier)
+        self.password.fill(password, identifier=self.identifier)
 
     def check_visible(self):
-        expect(self.email).to_be_visible()
-        expect(self.email).to_have_text('user@gmail.com')
-        expect(self.username).to_be_visible()
-        expect(self.username).to_have_text('username')
-        expect(self.password).to_be_visible()
-        expect(self.password).to_have_text('password')
+        self.email.check_visible(identifier=self.identifier)
+        self.email.check_have_text("user@gmail.com", identifier=self.identifier)
+
+        self.username.check_visible(identifier=self.identifier)
+        self.username.check_have_text("username", identifier=self.identifier)
+
+        self.password.check_visible(identifier=self.identifier)
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,3 +1,6 @@
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 from pages.base_page import BasePage
 from playwright.sync_api import Page
 from components.authentication.registration_form_component import RegistrationFormComponent
@@ -7,9 +10,10 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
         self.registration_form = RegistrationFormComponent(page, 'registration-form')
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.login_link = page.get_by_test_id('registration-page-login-link')
-        self.course_title = page.get_by_test_id('authentication-ui-course-title-text')
+
+        self.registration_button = Button(page, 'registration-page-registration-button','Registration')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
+        self.course_title =Text(page, 'authentication-ui-course-title-text', 'UI Course')
 
     def click_registration_button(self):
         self.registration_button.click()
